107eb09f3037170001e601b832c45664b9937ccc
SEA-001 <sea-001@SEA-001s-MacBook-Pro.local>
2025-05-23 09:14:45 +0700

    fix change background

--- assets/Scripts/SpriteController.js
+++ assets/Scripts/SpriteController.js
@@ -27,7 +27,7 @@ cc.Class({
 
     onChangeSprite: function() {
         if (this.flag) {
-            this.node1.getComponent(cc.Sprite).spriteFrame = this.spriteComponent1;
+            this.node1.getComponent(cc.Sprite).spriteFrame = this.spriteComponent2;
             console.log("if");
             this.flag = false;
         }else if (!this.flag) {
