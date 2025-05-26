aefdc69da6115ed890fcf8468dfe044c03e7a576
AnNguyen1998 <conandold@gmail.com>
2025-05-26 10:56:01 +0700

    done practice 2

--- assets/Scripts/Lobby/TableController.js
+++ assets/Scripts/Lobby/TableController.js
@@ -13,17 +13,61 @@ cc.Class({
     },
 
     onLoad() {
-        const fakeData = ['PixelProwler', 'ByteBlade', 'VortexViper', 'AuraAssassin', 'CircuitSorcerer', 'EchoEnigma', 'GlyphGhost', 'NebulaNomad', 'QuantumQuasar', 'ZenithZephyr'];
+        const fakeData = [
+            {
+                username: 'PixelProwler',
+                point: 1500
+            },
+            {
+                username: 'ByteBlade',
+                point: 1450
+            },
+            {
+                username: 'VortexViper',
+                point: 950
+            },
+            {
+                username: 'AuraAssassin',
+                point: 1120
+            },
+            {
+                username: 'CircuitSorcerer',
+                point: 1025
+            },
+            {
+                username: 'EchoEnigma',
+                point: 520
+            },
+            {
+                username: 'GlyphGhost',
+                point: 980
+            },
+            {
+                username: 'NebulaNomad',
+                point: 1250
+            },
+            {
+                username: 'QuantumQuasar',
+                point: 1110
+            },
+            {
+                username: 'ZenithZephyr',
+                point: 990
+            }];
         const cellList = [];
         for (let i = 0; i < 10; i++) {
             let cell = cc.instantiate(this.cell);
             cellList.push(cell);
             cell.parent = this.layoutTable.node;
         }
+        fakeData.sort((a, b) => b.point - a.point);
         fakeData.forEach((data, index) => {
             let labelName = cellList[index].getChildByName('Name').getComponent(cc.Label);
             let labelRank = cellList[index].getChildByName('Rank').getComponent(cc.Label);
-            labelName.string = data;
+            let titlePoint = cellList[index].getChildByName('TitlePoint');
+            let labelPoint = titlePoint.getChildByName('Point').getComponent(cc.Label);
+            labelPoint.string = data.point + ' points';
+            labelName.string = data.username;
             labelRank.string = index + 1;
         });
         cellList[0].color = cc.Color.RED;
